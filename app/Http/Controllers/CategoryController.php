8e51a3f44474ea863793deaa4efab87945cb8164
nicolasjveiga <nickjveiga@gmail.com>
2025-07-11 08:57:32 -0300

    ✨ Implemented update, read, and delete functionalities for categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Services\CategoryService;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
@@ -15,6 +17,20 @@ class CategoryController extends Controller
         $this->categoryService = $categoryService;
     }
 
+    public function index()
+    {
+        $categories = Category::with('products')->get();
+
+        return response()->json($categories);
+    }
+
+    public function show(Category $category)
+    {
+        $category->load('products'); 
+
+        return response()->json($category);
+    }
+
     public function store(StoreCategoryRequest $request)
     {
         $validated = $request->validated();
@@ -22,5 +38,21 @@ class CategoryController extends Controller
         $category = $this->categoryService->createCategory($validated);
 
         return response()->json($category, 201);
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $validated = $request->validated();
+
+        $category = $this->categoryService->updateCategory($category, $validated);
+
+        return response()->json($category);
+    }
+
+    public function destroy(Category $category)
+    {
+        $this->categoryService->deleteCategory($category);
+
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,24 +8,22 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Middleware\CheckRole;
 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+Route::middleware(['auth:sanctum', 'role:ADMIN'])->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+});
 
 Route::middleware(['auth:sanctum', 'role:ADMIN,MODERATOR'])->group(function (){
     Route::get('/adminmoderator', function () {
         return response()->json(['message' => 'Access granted to ADMIN and MODERATOR']);
     });
-});
-
-Route::middleware(['auth:sanctum', 'role:ADMIN'])->group(function () {
-    Route::get('/admin', function () {
-            return response()->json(['message' => 'Access granted to ADMIN']);
-        });
 });
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -34,8 +32,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/addresses/{address}', [AddressController::class, 'show']);
     Route::put('/addresses/{address}', [AddressController::class, 'update']);
     Route::delete('/addresses/{address}', [AddressController::class, 'destroy']);
-
-    Route::post('/products', [ProductController::class, 'store']);
-
-    Route::post('/categories', [CategoryController::class, 'store']);
 });
